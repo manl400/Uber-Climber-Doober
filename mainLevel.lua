b732f7e2bf11a3d98eba6e93412a5755a7d5c86a
manl400 <movie.edit@yahoo.com>
2017-12-12 00:45:36 -0600

    Added background

--- mainLevel.lua
+++ mainLevel.lua
@@ -2,6 +2,7 @@
 --
 -- mainLevel.lua
 --
+-- Notes: Add more gravity after jump for a moment
 -----------------------------------------------------------------------------------------
 
 local composer = require( "composer" )
@@ -10,31 +11,30 @@ local scene = composer.newScene()
 -- include Corona's "physics" library
 local physics = require "physics"
 
-physics.setDrawMode( "hybrid" )
+physics.setDrawMode( "normal" )
 
 local playerDirectionRight = -1 
 local playerDirectionLeft = 1
 
 local Doober
+local Background1, Background2, Background3
 local leftMove, rightMove, jumpMove
-local background, GroundTemp
+local background, ground
 
 local motionx = 0
 local dooberSpeed = 5
-
-
 
 local DooberForceX = 0.0 --might not need these too
 local DooberForceY = 0.0
 local MaxSpeedX = 8.0
 local MaxSpeedY = 7.0
 local SpeedAmount = 1.0
-local JumpGravity = 1
 
 local platform_bound = { -60, -15, 60, -15, -60, 15, 60, 15  }
 
  -- The platforms should be put in this array
 local Array_of_Platforms = {}
+local Array_of_Backgrounds = {}
 
 -- This is what is added to change the Y of Everthing for pans.
 local OFFSETY = 0; -- This is changed by something else a hit box at the top and the bottom that and to this to keep player on screen
@@ -42,15 +42,19 @@ local OFFSETY = 0; -- This is changed by something else a hit box at the top and
 
 local function New_Platform (x,y)
 
--- put code in here that makes a new platfor and shove it into the array with it's x and y;
-local NewPlatform
+	-- put code in here that makes a new platfor and shove it into the array with it's x and y;
+	local NewPlatform
 
 	NewPlatform = display.newRect(x, y, 120, 30)
-	physics.addBody(NewPlatform, "static", {friction=0.5}, {box= {halfWidth=60, halfHeight=10, x=0, y=15}, isSensor = true} )
-	
-table.insert(Array_of_Platforms,NewPlatform)
+	physics.addBody(NewPlatform, "static", {friction=0.5})
+  
+  NewPlatform.objType = "ground"
+  
+	table.insert(Array_of_Platforms,NewPlatform)
 
 end
+
+
 
 
 -- global variables
@@ -60,8 +64,8 @@ bottomSectionSize = 350 -- This is for the height of left and right touch areas
 -- Gradient color
 local backgroundGrad = {
 	type = "gradient",
-	color2 = {0.1, 0.6, 1, 0.9},
-	color1 = {0.3, 0.4, 1, 1},
+	color2 = {0/255, 178/255, 237/255, 1},
+	color1 = {0/255, 99/255, 237/255, 0.7},
 	direction = "up"
 }
 
@@ -160,6 +164,33 @@ local dooberSpriteSheet =
 	}
 }
 
+
+local backgroundSpriteSheet = 
+{
+  frames = 
+  {
+    { -- Tower1
+      x = 0,
+      y = 1100,
+      width = 720,
+      height = 1080
+    },
+     { -- Tower1
+      x = 0,
+      y = 1100,
+      width = 720,
+      height = 760
+    },
+     { -- Tower2
+      x = 0,
+      y = 0,
+      width = 720,
+      height = 1079
+    }
+  }
+}
+
+local backgroundSheet = graphics.newImageSheet("TowerSprite20px_Split.png", backgroundSpriteSheet)
 local dooberSheet = graphics.newImageSheet("Spritesheet.png", dooberSpriteSheet)
 
 
@@ -186,21 +217,34 @@ local seq_doober = {
 local idleDooberCharacter, runningDooberCharacter
 -------------------------------------
 
+function dooberCollide(self, event)
+  if ( event.selfElement == 2 and event.other.objType == "ground" ) then
+      if ( event.phase == "began" and self.goThrough == false ) then
+         self.goThrough = true
+      elseif ( event.phase == "ended" and self.goThrough == true ) then
+         self.goThrough = false
+      end
+   end
+end
+
 --Create Doober
 local function makeDoober()
 	doober = display.newSprite(dooberSheet, seq_doober)
 	doober.x = display.actualContentWidth/2
-	doober.y = 350
+	doober.y = 1000
 	doober.rightMove = 0
 	doober.leftMove = 0
-	local boundery = { -17, -35, 17, -35, -15, 30, 13, 30  }
-	physics.addBody(doober, "dynamic", {shape = boundery, friction=0.3})
+	local boundery = { -17, -35, 17, -35, -15, 35, 13, 35  }
+	physics.addBody(doober, "dynamic", {shape = boundery, density=4,  friction=0.3}, {box= {halfWidth = 25, halfHeight=10, x=0, y=-30}, isSensor = true} )
 	
 	doober.isFixedRotation = true
+  doober.objType = "player"
+  doober.goThrough = false
+  
+  doober.collision = dooberCollide
+  doober:addEventListener( "collision", doober )
 end	
-
 -------------------------------------
-
 
 
 
@@ -237,7 +281,11 @@ local function movePlayer(event)
 end
 -------------------------------------
 
-
+local function dooberGoThrough()
+  if doober.goThrough == true then
+    
+  end
+end
 
 -- Touch Listeners (Where stuff happens when clicking the left, right, and jump area)
 local function touchListener (event) 
@@ -273,11 +321,11 @@ local function touchListener (event)
 end
 
 local function jumpListener (event) -- Haven't finished this, can be modeled after touchListener
-	
-	print("jumpMove!")
-	doober:setLinearVelocity(0, -200)
-	--DooberForceY = 25;
-	
+  if(event.phase == "began") then
+    print("jumpMove!")
+    doober:applyLinearImpulse( 0, -270, doober.x, doober.y )
+    OFFSETY = 1
+  end
 	return true
 end
 -------------------------------------
@@ -322,17 +370,62 @@ local function createTempBackground()
 	background.anchorY = 0	
 end
 
-local function createTempPlatform() -- use as ground
-	GroundTemp = display.newRect(365, 1000, 6000, 30)
-	physics.addBody(GroundTemp, "static", {friction=0.5})
+local function createGround() -- use as ground
+	ground = display.newImageRect(backgroundSheet, 1, 720, 1080) 
+  ground.anchorX = 0
+	ground.anchorY = 0	
+  ground.x = 0
+  ground.y = display.actualContentHeight - 1080
+  print(ground.y)
+  local boundery = {-460, 385, 720, 385, -460, 540, 720, 540  }
+	physics.addBody(ground, "static", {shape = boundery, friction=0.5})
+end
+
+local function createBackground()
+  
+  Background1= display.newImageRect(backgroundSheet, 3, 720, 1079)
+  Background2= display.newImageRect(backgroundSheet, 3, 720, 1079)
+  Background3= display.newImageRect(backgroundSheet, 3, 720, 1079)
+  Background1.anchorX = 0
+	Background1.anchorY = 0
+  Background2.anchorX = 0
+	Background2.anchorY = 0
+  Background3.anchorX = 0
+	Background3.anchorY = 0
+  
+  Background1.x = 0
+  Background1.y = ground.y - Background1.contentHeight
+
+  
+  Background2.x = 0
+  Background2.y = Background1.y - Background2.contentHeight
+
+  Background3.x = 0
+  Background3.y = Background2.y - Background3.contentHeight 
+  
 end
 -------------------------------------
+
+local function moveBackground()
+  Background1.y = Background1.y + OFFSETY
+  Background2.y = Background2.y + OFFSETY
+  Background3.y = Background3.y + OFFSETY
+  
+  if (Background1.y) > 1400 then
+  Background1:translate(0,-3237)
+  end
+  if (Background2.y) > 1400 then
+   Background2:translate(0,-3237)
+  end
+  if (Background3.y) > 1400 then
+   Background3:translate(0,-3237)
+  end
+end 
 
 
 -- Game LOOP
 
 local function gameLoop()
-
 
 	for i = 1 , table.getn(Array_of_Platforms) do
 	
@@ -366,53 +459,37 @@ local MinX = 100;
 local MaxX = 600; 
 local Spawn_DIFF = 180;
 
-OFFSETY = 0;
-
-
+moveBackground()
 -- update Offset (for panning)
 -- Needs to be size of loop. idk if arrays start at y
 for i = 1, table.getn(Array_of_Platforms) do
-
-
-    -- This will push its Y for cam track
-    Array_of_Platforms[i].y = Array_of_Platforms[i].y + OFFSETY
-    
-    -- Also do player cam update as well.
+  -- This will push its Y for cam track
+  Array_of_Platforms[i].y = Array_of_Platforms[i].y + OFFSETY
+  -- Also do player cam update as well.
 	if i == 1 then
-	
     doober.y = doober.y + OFFSETY
-	
-	GroundTemp.y = GroundTemp.y + OFFSETY
-    
+    ground.y = ground.y + OFFSETY
 	end
 
 end
 
-    OFFSETY = 0;
 
 -- Need to get the value of Y for the last platfor in the array
 if Array_of_Platforms[#Array_of_Platforms].y>Spawn_DIFF then
 
--- use this for finding points to spawn
-local Offset_off = 100;
-local Array_OF_Points = {MinX, (MinX+100)*1, (MinX+100)*2, (MinX+100)*3, (MinX+100)*4}
+  -- use this for finding points to spawn
+  local Offset_off = 100;
+  local Array_OF_Points = {MinX, (MinX+100)*1, (MinX+100)*2, (MinX+100)*3, (MinX+100)*4}
 
+  local HowManyToSpawn = math.random(3)
 
-local HowManyToSpawn = math.random(3)
+  for i = 1, HowManyToSpawn do
+    if 2 == math.random(2) then
 
-for i = 1, HowManyToSpawn do
-
-	if 2 == math.random(2) then
-
-
-
-        local TargetSpawn = math.random (5)
-        --makes a platforms this should work but that funtion will need setup see above
-        New_Platform(Array_OF_Points[TargetSpawn],-30)
-    
-    
-
-    end
+    local TargetSpawn = math.random (5)
+    --makes a platforms this should work but that funtion will need setup see above
+    New_Platform(Array_OF_Points[TargetSpawn],-30)
+  end
 
 end
 
@@ -454,9 +531,10 @@ function scene:create( event )
 	-- Start Physics, Physics needs to start and stop to create physics bodys
 	physics.start()
 	physics.pause()
+  physics.setGravity(0, 9.8*3)
 	
 	createTempBackground()
-	createTempPlatform()
+	createGround()
 	
 	createLeftMoveArea()
 	createRightMoveArea()
@@ -471,10 +549,14 @@ function scene:create( event )
 	
 	
 	makeDoober()
-	
+  createBackground()
+  
 	sceneGroup:insert( background )
+  sceneGroup:insert( Background1 )
+  sceneGroup:insert( Background2 )
+  sceneGroup:insert( Background3 )
 	sceneGroup:insert( doober )
-	sceneGroup:insert( GroundTemp )
+	sceneGroup:insert( ground )
 	
 	
 	doober:toFront()
